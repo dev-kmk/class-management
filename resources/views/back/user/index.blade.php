f07c44819871c6d1e7f20620efe251562f308342
Kaung Myat Kyaw <dev.kmkyaw@gmail.com>
2024-09-05 17:04:54 +0630

    updated

--- resources/views/back/user/index.blade.php
+++ resources/views/back/user/index.blade.php
@@ -61,13 +61,13 @@
                                         -
                                     @endif
                                 </div>
-                                <div class="col-md-3 d-flex column-gap-2">
+                                <div class="col-md-3">
                                     <form action="{{ route('users.destroy', $user->id) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
                                         <a href="#" class="btn btn-outline-success btn-sm" title="View"><i class="bi bi-eye"></i> View</a>
-                                        <a href="#" class="btn btn-outline-primary btn-sm" title="Edit"><i class="bi bi-pencil-square"></i> Edit</a>
-                                        <button class="btn btn-danger btn-sm" title="Delete" onclick="return confirm('Are you sure you want to delete?')"><i class="bi bi-trash3"></i> Delete</button>
+                                        <a href="#" class="btn btn-outline-primary btn-sm ms-2" title="Edit"><i class="bi bi-pencil-square"></i> Edit</a>
+                                        <button class="btn btn-danger btn-sm ms-2" title="Delete" onclick="return confirm('Are you sure you want to delete?')"><i class="bi bi-trash3"></i> Delete</button>
                                     </form>
                                 </div>
                             </div>
